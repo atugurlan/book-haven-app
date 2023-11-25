e8dd12066eab3bc4eec9522c1fdcd21459eef6f9
atugurlan <alexandra.tugurlan@student.upt.ro>
2023-11-25 21:12:10 +0200

    created the registration with email and password

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCpNs6wT8mgvWjVC8SQJKlWSvhFwOslB-M",
+        authDomain: "book-haven-cfbba.firebaseapp.com",
+        projectId: "book-haven-cfbba",
+        storageBucket: "book-haven-cfbba.appspot.com",
+        messagingSenderId: "787824566791",
+        appId: "1:787824566791:web:d1ae5ba65e823621b1c0a2",
+        measurementId: "G-TSS0BKLM2K"
+    },
+};
