8f5e0c092f3e79081dd875f80f2b8ceb5691f2bb
atugurlan <alexandra.tugurlan@student.upt.ro>
2024-01-01 13:42:15 +0200

    made changes such that when the quantity of a book changes, the shopping cart item changes accordingly

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
-import { from } from 'rxjs';
+import { Subscription, from, interval } from 'rxjs';
 import { Cart } from 'src/app/models/cart';
 import { CartItem } from 'src/app/models/cart-item';
 import { Order } from 'src/app/models/order';
 import { ProfileUser } from 'src/app/models/user-profile';
+import { BookService } from 'src/app/services/book/book.service';
 import { CartService } from 'src/app/services/cart/cart-service.service';
 import { OrderService } from 'src/app/services/order/order.service';
 import { UsersService } from 'src/app/services/users/users.service';
@@ -18,20 +19,60 @@ export class ShoppingCartComponent implements OnInit {
   cart!:Cart;
   cartItems!:CartItem[];
   orders!:Order[];
+  private updateSubscription: Subscription;
 
   user$ = this.usersService.currentUserProfile$;
 
   constructor(private usersService: UsersService,
               private cartService: CartService,
               private firestore: Firestore,
-              private ordersService: OrderService) { }
+              private ordersService: OrderService,
+              private bookService: BookService) {
+                this.updateSubscription = interval(5000).subscribe(() => {
+                  this.checkQuantity();
+                });
+              }
   
   async ngOnInit(): Promise<void> {
     this.cart = await this.cartService.getCart();
+    this.cartItems = this.cart.items || [];
+  }
+
+  ngOnDestroy(): void {
+    this.updateSubscription.unsubscribe();
   }
 
   async setCart() {
     this.cart = await this.cartService.getCart();
+  }
+
+  async maximumQuantity(cartItem: CartItem):Promise<boolean> {
+    const books = await this.bookService.allBooks();
+
+    const book = books.find((b) => b.bid === cartItem.book.bid) || null;
+
+    if (book) {
+      if (cartItem.quantity >= book.quantity) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  async checkQuantity() {
+    const books = await this.bookService.allBooks();
+
+    for (const cartItem of this.cartItems) {
+      const book = books.find((b) => b.bid === cartItem.book.bid) || null;
+
+      if (book) {
+        if (cartItem.quantity > book.quantity) {
+          console.log(`Updating quantity for book ${book.bid}`);
+          cartItem.quantity = book.quantity;
+        }
+      }
+    }
   }
 
   decreaseQuantity(cartItem: CartItem) {
@@ -39,12 +80,14 @@ export class ShoppingCartComponent implements OnInit {
     if (cartItem.quantity > 1) {
       quantity = cartItem.quantity - 1;
       this.updateCart(cartItem, quantity);
+      this.checkQuantity()
     }
   }
 
   increaseQuantity(cartItem: CartItem) {
     let quantity = cartItem.quantity + 1;
     this.updateCart(cartItem, quantity);
+    this.checkQuantity()
   }
 
   async removeFromCart(cartItem:CartItem) {
