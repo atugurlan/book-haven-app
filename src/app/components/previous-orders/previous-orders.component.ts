44ded45dabeac00e1969ca5340319bd7a6046d07
atugurlan <alexandra.tugurlan@student.upt.ro>
2024-01-04 09:20:32 +0200

    created a simple view of a customer's previous orders

---
+++ src/app/components/previous-orders/previous-orders.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Order } from 'src/app/models/order';
+import { OrderService } from 'src/app/services/order/order.service';
+import { UsersService } from 'src/app/services/users/users.service';
+
+@Component({
+  selector: 'app-previous-orders',
+  templateUrl: './previous-orders.component.html',
+  styleUrls: ['./previous-orders.component.css']
+})
+export class PreviousOrdersComponent {
+  user$ = this.usersService.currentUserProfile$;
+  customerOrder!:Order[];
+  
+  constructor(private usersService: UsersService, private ordersService: OrderService) {
+    this.getCustomerOrders();
+   }
+
+  async getCustomerOrders() {
+    this.user$.subscribe( async (user) => {
+      if(user) {
+        this.customerOrder = await this.ordersService.getAllCustomerOrders(user.uid);
+        console.log(this.customerOrder);
+      }
+      return this.customerOrder;
+    });
+  }
+}
--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -60,4 +60,11 @@ export class OrderService {
         console.error('Error adding order:', error);
     });
   }
+
+  async getAllCustomerOrders(id:string):Promise<Order[]> {
+    const orders = await this.getAllOrders();
+    let customerOrders = orders.filter(order => order.customer == id);
+    console.log(customerOrders);
+    return customerOrders;
+  }
 }
